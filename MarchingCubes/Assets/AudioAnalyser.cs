8516ef54ddcd63898289c240707d6c9ed506caed
CaseyOgbevoen <C16315013@mydit.ie>
2019-12-13 14:28:02 +0000

    cube grid + some audio visualisation code

---
+++ MarchingCubes/Assets/AudioAnalyser.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioAnalyser : MonoBehaviour
+{
+    public bool useMic = false;
+    public AudioClip clip;
+    AudioSource a;
+    public AudioMixerGroup amgMic;
+    public AudioMixerGroup amgMaster;
+
+    public string selectedDevice;
+
+    public int frameSize = 512;
+    public static float[] bands;
+    public static float[] spectrum;
+
+    public float binWidth;
+    public float sampleRate;
+
+    private void Awake()
+    {
+        a = GetComponent<AudioSource>();
+        spectrum = new float[frameSize];
+        bands = new float[(int)Mathf.Log(frameSize, 2)];
+
+        if (useMic)
+        {
+            if (Microphone.devices.Length > 0)
+            {
+                selectedDevice = Microphone.devices[0].ToString();
+                a.clip = Microphone.Start(selectedDevice, true, 1, AudioSettings.outputSampleRate);
+
+                a.outputAudioMixerGroup = amgMic;
+            }
+        }
+        else
+        {
+            a.clip = clip;
+            a.outputAudioMixerGroup = amgMaster;
+        }
+        a.Play();
+    }
+
+    void GetFrequencyBands()
+    {
+        for (int i = 0; i < bands.Length; i++)
+        {
+            int start = (int)Mathf.Pow(2, i) - 1;
+            int width = (int)Mathf.Pow(2, i);
+            int end = start + width;
+            float average = 0;
+            for (int j = start; j < end; j++)
+            {
+                average += spectrum[j] * (j + 1);
+            }
+            average /= (float)width;
+            bands[i] = average;
+            //Debug.Log(i + "\t" + start + "\t" + end + "\t" + start * binWidth + "\t" + (end * binWidth));
+        }
+
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        sampleRate = AudioSettings.outputSampleRate;
+        binWidth = AudioSettings.outputSampleRate / 2 / frameSize;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        a.GetSpectrumData(spectrum, 0, FFTWindow.Blackman);
+        GetFrequencyBands();
+    }
+}
---
+++ MarchingCubes/Assets/Cubes.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cubes : MonoBehaviour
+{
+    //dimensions of cubes
+    public int width = 20;
+    public int height = 20;
+
+    //vislualiser scale
+    public float scale = 10;
+
+    //list of game objects
+    List<GameObject> elements = new List<GameObject>();
+
+    //create vislualisers function
+    void CreateVisualisers()
+    {
+        float theta = (Mathf.PI * 2.0f) / (float)AudioAnalyser.bands.Length;
+        for (int i = 0; i < AudioAnalyser.bands.Length; i++)
+        {
+            //Vector3 pos = new Vector3(Mathf.Sin(theta * i) * radius, 0, Mathf.Cos(theta * i) * radius);
+            //pos = transform.TransformPoint(pos);
+
+            //Quaternion (rotation)
+            //Quaternion q = Quaternion.AngleAxis(theta * i * Mathf.Rad2Deg, Vector3.up);
+            //q = transform.rotation * q;
+
+
+            //cube grid
+            int halfWidth = width / 2;
+
+            for (int j = 0; j < height; j++)
+            {
+                for (int k = -halfWidth; k < halfWidth; k++)
+                {
+                    //position cubes on top of quad
+                    Vector3 pos = transform.TransformPoint(new Vector3(k, 1, 0.5f + j));
+
+                    //create cube
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                    //position cube
+                    cube.transform.position = pos;
+                    cube.transform.rotation = transform.rotation;
+
+                    //color cube
+                    cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(i / (float)AudioAnalyser.bands.Length, 1, 1);
+
+                    //Parent
+                    cube.transform.parent = this.transform;
+
+                    //make rigid body
+                    cube.AddComponent<Rigidbody>();
+
+                    //add to elements list
+                    elements.Add(cube);
+                }
+            }
+
+
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateVisualisers();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    //creating grid of cubes
+    /* 
+     int halfWidth = width / 2;
+
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = -halfWidth; j < halfWidth; j++)
+            {
+                //position cubes on top of quad
+                Vector3 pos = transform.TransformPoint(new Vector3(j, 1, 0.5f + i));
+
+                //create cube
+                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+                //position cube
+                cube.transform.position = pos;
+                cube.transform.rotation = transform.rotation;
+
+                //color cube
+                cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.value, 1, 1);
+
+                //Parent
+                cube.transform.parent = this.transform;
+
+                //make rigid body
+                cube.AddComponent<Rigidbody>();
+            }
+        }
+     */
+}
