cddadabff7d2c6b9d25ec422549edaf4f0774703
CaseyOgbevoen <C16315013@mydit.ie>
2019-12-14 22:49:20 +0000

    Update Cubes.cs

--- MarchingCubes/Assets/Cubes.cs
+++ MarchingCubes/Assets/Cubes.cs
@@ -14,11 +14,11 @@ public class Cubes : MonoBehaviour
     //number of rows to generate
     //public int numRows = 10;
 
+    //spawn speed
+    public float spawnSpeed = 3;
+
     //vislualiser scale
     public float scale = 10;
-
-    //radius
-    public float radius = 50;
 
     //list of game objects
     List<GameObject> elements = new List<GameObject>();
@@ -26,43 +26,46 @@ public class Cubes : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        CreateVisualisers();
+        StartCoroutine(CreateVisualisers());
     }
 
     //create vislualisers function
-    void CreateVisualisers()
+    IEnumerator CreateVisualisers()
     {
         //set position
         Vector3 startPos = transform.position;
-        startPos.z = 100;
+        startPos.z = 0;
 
         Vector3 size = new Vector3(1, 1, 9);
 
-        for (int y = 0; y < size.y; y++)
+        for (int n = 0; n < 10; n++)
         {
-            for (int z = 0; z < size.z; z++)
+            for (int y = 0; y < size.y; y++)
             {
+                for (int z = 0; z < size.z; z++)
+                {
+                    //spawn cube
+                    GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                    cube.transform.position = new Vector3(startPos.z, y * size.y, z * size.z);
 
-                //spawn cube
-                GameObject cube = GameObject.CreatePrimitive(PrimitiveType.Cube);
-                cube.transform.position = new Vector3(startPos.z, y * size.y, z * size.z);
+                    //parent
+                    cube.transform.parent = this.transform;
 
-                //parent
-                cube.transform.parent = this.transform;
+                    //color
+                    cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.value, 1, 1);
+                    elements.Add(cube);
 
-                //color
-                cube.GetComponent<Renderer>().material.color = Color.HSVToRGB(Random.value, 1, 1);
-                elements.Add(cube);
+                    startPos.z++;
+                }
             }
+            yield return new WaitForSeconds(spawnSpeed);
         }
-        startPos.z = startPos.z++;
-      
     }
 
     // Update is called once per frame
     void Update()
     {
-        for (int i = 0; i < elements.Count; i++)
+        for (int i = 0; i < 1000; i++)
         {
             Vector3 ls = elements[i].transform.localScale;
             ls.y = Mathf.Lerp(ls.y, 1 + (AudioAnalyser.bands[i] * scale), Time.deltaTime * 10.0f);
