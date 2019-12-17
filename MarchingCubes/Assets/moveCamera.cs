e543eab3045897ccae2f3d33a55d21051df32534
CaseyOgbevoen <C16315013@mydit.ie>
2019-12-17 14:40:57 +0000

    camera movement

--- MarchingCubes/Assets/moveCamera.cs
+++ MarchingCubes/Assets/moveCamera.cs
@@ -5,20 +5,22 @@ using UnityEngine;
 public class moveCamera : MonoBehaviour
 {
 
-    //smooth speed
-    public float smoothSpeed = 0.125f;
-
     //speed
-    public float speed = 3f;
-
-    //waypoint list
-    public List<Vector3> waypoints = new List<Vector3>();
-
+    public float speed = 1f;
 
     // Start is called before the first frame update
     void Start()
     {
-     
+        /*/camera start position
+        Vector3 pos = new Vector3(-40, 20, 30);
+
+        //camera start rotation
+        Vector3 rot = new Vector3(5, 90, (float) -2.5);
+        */
+
+        //transform
+        //transform.position = new Vector3(-10, 10, 100);
+        //transform.rotation = new Quaternion(0, 180, (float)10, 1);
     }
 
     void LateUpdate()
