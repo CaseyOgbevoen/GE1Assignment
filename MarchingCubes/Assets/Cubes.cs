429ccc83980fa818751173df5c63d4aeafbdeb54
CaseyOgbevoen <C16315013@mydit.ie>
2019-12-18 16:32:19 +0000

    Update Cubes.cs

--- MarchingCubes/Assets/Cubes.cs
+++ MarchingCubes/Assets/Cubes.cs
@@ -11,7 +11,7 @@ public class Cubes : MonoBehaviour
     public float spawnSpeed = 1.5f;
 
     //vislualiser scale
-    public float scale = 2f;
+    public float scale = 2.5f;
 
     //array of game objects
     public GameObject [] elements;
